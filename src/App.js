8514010003dfb9b5ee625a7fe1c40d59bf655cca
tahmdev <qwertiimusic@gmail.com>
2022-03-31 17:48:52 +0200

    snake 1.0

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
 import { useEffect, useMemo, useState } from 'react';
+import 'font-awesome/css/font-awesome.min.css';
+
+
+let intervalId = null
 
 function App() {
+  const [timer, setTimer] = useState(0)
+  if (intervalId) {
+    clearInterval(intervalId);
+  }
+  intervalId = setInterval(() => {  
+    let newTimer = timer +1
+    setTimer(newTimer)
+  }, 140);
+    
   return (
     <div className="App">
-      <Arena />
+      <Arena timer={timer}/>
     </div>
   );
 }
 
-const Arena = () => {
-  const [cellAmount, setCellAmount] = useState(5)
+const Arena = (props) => {
+  const [cellAmount, setCellAmount] = useState(10)
   const [cellArray, setCellArray] = useState([])
   const [activeCells, setActiveCells] = useState([[2,3]])
   const [foodCell, setFoodCell] = useState([0,0])
@@ -34,11 +47,15 @@ const Arena = () => {
     gridTemplateRows: `repeat(${cellAmount}, auto)`,
   }
 
-  //Move into move function later
+  //Display movement & handle collision
   useEffect(() => {
     activeCells.map((item, idx) => {
       let cell = document.getElementById(`x${item[0]}y${item[1]}`);
       //handle body movement
+      if (!cell){
+        console.log("ded")
+        resetGame();
+      }
       if (cell && idx !== 0) {
         cell.classList.add("active")
         cell.classList.remove("empty")
@@ -52,12 +69,12 @@ const Arena = () => {
         if (cell.classList.contains("head") && cell.classList.contains("active")){
           resetGame();
         }
-      }
+      } 
     })
     let head = document.getElementById(`x${activeCells[0][0]}y${activeCells[0][1]}`);
     
     //detect food collision
-    if (head.classList.contains("food")){
+    if (head && head.classList.contains("food")){
       head.classList.remove("food")
       setAteFood(true)
       let emptyCells = document.getElementsByClassName("empty")
@@ -85,29 +102,38 @@ const Arena = () => {
   }
   return(
     <div className="arena-wrapper">
-      <input id="cell-numers-temp"type="number" onChange={ e => e.target.value > 0 ? setCellAmount(e.target.value) :null} />
-      <div className="grid" style={gridStyle}>
-        {cellArray.map(item => {
-          return(
-            <div key={`x${item[0]}y${item[1]}`} id={`x${item[0]}y${item[1]}`} className="cell empty"></div>
-          )
-        })}
+      <div className="grid-wrapper">
+        <div className="grid" style={gridStyle}>
+          {cellArray.map(item => {
+            return(
+              <div key={`x${item[0]}y${item[1]}`} id={`x${item[0]}y${item[1]}`} className="cell empty"></div>
+            )
+          })}
+        </div>
       </div>
       <Controls
       activeCells = {activeCells}
       setActiveCells = {setActiveCells}
       ateFood = {ateFood}
       setAteFood = {setAteFood}
+      timer = {props.timer}
+      setCellAmount = {setCellAmount}
+      cellAmount = {cellAmount}
       />
     </div>
   )
 }
 
+
+
 const Controls = (props) =>{
-  
-  const handleClick = (e) => {
+  let [direction, setDirection] = useState("a")
+  //prevents going backwards
+  let [lastDirection, setlastDirection] = useState("")
+
+
+  const move = () => {
     let newArr = [...props.activeCells]
-    
     if(props.ateFood){
       props.setAteFood(false)
     }else{
@@ -116,26 +142,49 @@ const Controls = (props) =>{
       document.getElementById(`x${tail[0]}y${tail[1]}`).classList.remove("active")
       document.getElementById(`x${tail[0]}y${tail[1]}`).classList.add("empty")
     }
-    
-    
     let head = [...props.activeCells[0]]
-    switch(e.target.id){
+    switch(direction){
       case "up":
         head[0] -=1;
+        setlastDirection("up")
         break;
       case "down":
         head[0] +=1;
+        setlastDirection("down")
         break;
       case "right":
         head[1] +=1;
+        setlastDirection("right")
         break;
       case "left":
         head[1] -=1;
+        setlastDirection("left")
+        break;
+      default:
         break;
     }
     props.setActiveCells([head, ...newArr])
   }
+  useEffect(() => {
+    move();
+  }, [props.timer])
+    
+  const handleClick = (e) => {
+    if (e.target.id === "up" && lastDirection !== "down"){
+      setDirection("up")
+    }
+    if (e.target.id === "right" && lastDirection !== "left") {
+      setDirection("right")
+    }
+    if (e.target.id === "down" && lastDirection !== "up"){ 
+      setDirection("down")
+    }
+    if (e.target.id === "left" && lastDirection !== "right"){ 
+      setDirection("left")
+    }
+  }
 
+  //keyboard functionality
   useEffect(() => {
     const handleKey = (e) => {
       if(e.key === "w" || e.key === "ArrowUp") document.getElementById("up").click();
@@ -149,10 +198,12 @@ const Controls = (props) =>{
 
   return(
     <div className='controlls-wrapper'>
-      <button id="up" onClick={handleClick}>up</button>
-      <button id="right" onClick={handleClick}>right</button>
-      <button id="down" onClick={handleClick}>down</button>
-      <button id="left" onClick={handleClick}>left</button>
+      <input id="cell-numbers"type="number" onChange={ e => e.target.value > 0 ? props.setCellAmount(e.target.value) :null} value={props.cellAmount}/>
+
+      <button id="up" onClick={handleClick} style={direction === "up" ? {backgroundColor: "rgba(141, 141, 141, 0.8)"} : null}>up</button>
+      <button id="right" onClick={handleClick} style={direction === "right" ? {backgroundColor: "rgba(141, 141, 141, 0.8)"} : null}>right</button>
+      <button id="down" onClick={handleClick} style={direction === "down" ? {backgroundColor: "rgba(141, 141, 141, 0.8)"} : null}>down</button>
+      <button id="left" onClick={handleClick} style={direction === "left" ? {backgroundColor: "rgba(141, 141, 141, 0.8)"} : null}>left</button>
     </div>
   )
 }
